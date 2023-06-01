928feb8849e9d3da5baa29c4f10c71ba518a50b8
ea-coding <92234415+ea-coding@users.noreply.github.com>
2023-05-31 23:29:59 -0700

    Expand lesson1

---
+++ lessons/lesson1/challenge1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Welcome to challenge 1!"
+echo "For this challenge, you must complete 5 tasks."
+# Find hidden file
+# Create a file with a certain name
+# Get file size of a certain file
+# Copy a file
+# Move a file
--- lessons/lesson1/lesson1.sh
+++ lessons/lesson1/lesson1.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Checks whether given command is what we expected
+check_command() {
+    local given_input="$1"
+    local expected_input="$2"
+    while [[ "$given_input" != "$expected_input" ]]; do
+        read -p "Oops. This is not the $expected_input command. Try again: " given_input
+    done
+    echo "Well done!"
+}
+
 # Welcome message
 read -p "Please enter your name: " name
 echo "Hello $name! Welcome to Bash Master!"
 read -p "Are you ready to learn some bash: " response
-echo "response is $response"
-
 while [[ "$response" != "yes" && "$response" != "Yes" ]]; do
     echo "Let's get some more excitement for bash learning!"
     read -p "Are you ready to learn some bash (respond with 'yes'): " response
 done
 echo "Amazing! Let's get the party started!"
+echo
 
 # Bash Background
 echo "Before we jump into some exercises, let us learn a little more about Bash."
@@ -22,8 +31,10 @@ sleep 2
 echo "In these tutorials, we will focus on Bash as a command language."
 sleep 2
 echo "In particular, we will learn how you can control your machine using Bash commands and how to write Bash scripts."
+echo
 sleep 3
 
+# Lesson 1 Logistics
 echo "Okay, let's get started!"
 sleep 2
 echo "In this lesson, we will talk about some useful Bash commands to control your machine."
@@ -33,7 +44,9 @@ sleep 2
 echo "'pwd', 'ls', 'cd', 'mkdir', 'touch', 'find', 'cp' and 'mv'"
 sleep 2
 echo "If you know these commands, feel free to skip to lesson 2."
+echo
 sleep 2
+
 
 # Bash Directory traversals and manipulation
 echo "Okay, let's get started!"
@@ -44,6 +57,12 @@ echo "To follow along this lesson, feel free to open up a different terminal."
 sleep 2
 echo "It is also fine to put your commands into the input prompt for our purposes."
 sleep 2
+echo "The input prompts will look as follows."
+sleep 2
+read -p "Type the command here: " command1
+echo "Got command $command1. Cool let's move on!"
+echo
+sleep 2
 
 # Bash pwd
 echo "Let's start with the 'pwd' command."
@@ -53,13 +72,57 @@ sleep 2
 echo "As the name suggests, it prints the current working directory."
 sleep 2
 read -p "Type the 'pwd' command: " pwd_command
+check_command "pwd" "$pwd_command"
 while [[ "$pwd_command" != "pwd" ]]; do
     read -p "Oops. This is not the pwd command. Try again: " pwd_command
 done
 pwd_output=$(pwd)
-sleep 1
+sleep 2
 echo "Wow that was so cool!"
+sleep 2
 echo "We figured out that you are currently in following directory: $pwd_output"
+echo
+sleep 2
 
 # Bash cd command
+echo "We have learned out first bash command: pwd. We can find out which directory we are in."
+sleep 2
+echo "But what if we want to change directories?"
+sleep 2
+echo "There is another command that will help us do that."
+sleep 2
+echo "It is the 'cd' command. cd stands for... change directory (another cool abbreviaton)."
+sleep 2
+echo "To get to the directory from which the current program is running, type the folowing command:"
+script_directory=$(dirname "$0")
+cd_command1="cd $script_directory"
+echo "$cd_command1"
+sleep 2
+read -p "Type the command here: " cd_command1_input
+sleep 2
+check_command "$cd_command1_input" "$cd_command1"
+sleep 2
+echo "Now we are in the directory lesson1!"
 
+
+
+
+# do pwd to see how directory changes as we traverse
+
+# cd in absolute paths 
+# cd one directory relative
+# do cd ..
+
+
+
+# Bash ls command
+# Try ls
+# Bash flags
+
+# Lesson 1 Challenge
+echo "Great! Now that you have completed lesson1, I have a challenge for you."
+sleep 2
+echo "In order to complete this challenge, please only use your terminal."
+sleep 2
+echo "Run the challenge1.sh file (with this command: 'sh challenge1.sh). Have fun!"
+sleep 2
